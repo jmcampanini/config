30b97d8b6ab95e669a8a4dd86ebdea2343b397c5
Javier Campanini <jmcampanini@gmail.com>
2017-05-18 01:03:01 -0400

    update safari settings

--- osx.sh
+++ osx.sh
@@ -14,11 +14,26 @@ defaults write NSGlobalDomain AppleInterfaceStyle Dark
 # Dock Settings
 # =============
 
-# auto hide the dock
-defaults write com.apple.dock autohide -int 1
+# Remove the auto-hiding Dock delay
+defaults write com.apple.dock autohide-delay -float 0
+
+# Remove the animation when hiding/showing the Dock
+defaults write com.apple.dock autohide-time-modifier -float 0
+
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
 
 # set the tile size of the dock
 defaults write com.apple.dock tilesize -int 36
+
+
+# ===========
+# Hot Corners
+# ===========
+
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-br-corner -int 5
+defaults write com.apple.dock wvous-br-modifier -int 0
 
 
 # ==============================
@@ -60,7 +75,7 @@ defaults write "Apple Global Domain" AppleShowScrollBars -string WhenScrolling
 defaults write "Apple Global Domain" AppleKeyboardUIMode -int 2
 
 # key repeat settings
-defaults write "Apple Global Domain" InitialKeyRepeat -int 15
+defaults write "Apple Global Domain" InitialKeyRepeat -int 10
 defaults write "Apple Global Domain" KeyRepeat -int 1
 defaults write "Apple Global Domain" "InitialKeyRepeat_Level_Saved" -int 6
 defaults write "Apple Global Domain" ApplePressAndHoldEnabled -bool false
@@ -82,23 +97,73 @@ defaults write "com.apple.menuextra.clock" IsAnalog -int 0
 # Safari Settings
 # ===============
 
-#defaults write "com.apple.Safari" AutoOpenSafeDownloads -int 0
-#defaults write "com.apple.Safari" AutoFillCreditCardData -int 0
-#defaults write "com.apple.Safari" AutoFillFromAddressBook -int 0
-#defaults write "com.apple.Safari" AutoFillMiscellaneousForms -int 0
-#defaults write "com.apple.Safari" ShowFullURLInSmartSearchField -int 1
-#defaults write "com.apple.Safari" WebKitTabToLinksPreferenceKey -int 1
-#defaults write "com.apple.Safari" IncludeDevelopMenu -int 1
-#defaults write "com.apple.Safari" HomePage -string ""
-#defaults write "com.apple.Safari" NewTabBehavior -int 1
-#defaults write "com.apple.Safari" NewWindowBehavior -int 1
+# Show the full URL in the address bar (note: this still hides the scheme)
+defaults write com.apple.Safari ShowFullURLInSmartSearchField -bool true
 
+# Set Safari’s home page to `about:blank` for faster loading
+defaults write com.apple.Safari HomePage -string "about:blank"
 
-# ==================
-# OmniFocus Settings
-# ==================
+# Prevent Safari from opening ‘safe’ files automatically after downloading
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
-#defaults write "com.omnigroup.OmniFocus2" QuickEntryHotKey -dict flags "command|shift|option|control" key 31
+# Allow hitting the Backspace key to go to the previous page in history
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2BackspaceKeyNavigationEnabled -bool true
+
+# Hide Safari’s bookmarks bar by default
+defaults write com.apple.Safari ShowFavoritesBar -bool false
+
+# Hide Safari’s sidebar in Top Sites
+defaults write com.apple.Safari ShowSidebarInTopSites -bool false
+
+# Disable Safari’s thumbnail cache for History and Top Sites
+defaults write com.apple.Safari DebugSnapshotsUpdatePolicy -int 2
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Remove useless icons from Safari’s bookmarks bar
+defaults write com.apple.Safari ProxiesInBookmarksBar "()"
+
+# Enable the Develop menu and the Web Inspector in Safari
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+
+# Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Enable continuous spellchecking
+defaults write com.apple.Safari WebContinuousSpellCheckingEnabled -bool true
+
+# Disable auto-correct
+defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+
+# Disable AutoFill
+defaults write com.apple.Safari AutoFillFromAddressBook -bool false
+defaults write com.apple.Safari AutoFillPasswords -bool false
+defaults write com.apple.Safari AutoFillCreditCardData -bool false
+defaults write com.apple.Safari AutoFillMiscellaneousForms -bool false
+
+# Warn about fraudulent websites
+defaults write com.apple.Safari WarnAboutFraudulentWebsites -bool true
+
+# Disable plug-ins
+defaults write com.apple.Safari WebKitPluginsEnabled -bool false
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2PluginsEnabled -bool false
+
+# Disable Java
+defaults write com.apple.Safari WebKitJavaEnabled -bool false
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
+
+# Block pop-up windows
+defaults write com.apple.Safari WebKitJavaScriptCanOpenWindowsAutomatically -bool false
+defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaScriptCanOpenWindowsAutomatically -bool false
+
+# Enable “Do Not Track”
+defaults write com.apple.Safari SendDoNotTrackHTTPHeader -bool true
+
+# Update extensions automatically
+defaults write com.apple.Safari InstallExtensionUpdatesAutomatically -bool true
 
 
 # ==========
